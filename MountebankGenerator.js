7e6db06b52d3180b165aa6a30ef3a2df88e1c882
Ryan McCuaig <ryan@ryanmccuaig.net>
2016-04-19 11:28:42 -0700

    Sorta works

--- MountebankGenerator.js
+++ MountebankGenerator.js
@@ -1,8 +1,48 @@
+function pathFromUrl(url) {
+  var path;
+  path = url.replace(/^https?:\/\/[^\/]+/i, '');
+  if (!path) {
+    path = '/';
+  }
+  return path;
+};
+
+function generateEmpty () {
+  return {
+    predicates: [ ],
+    responses: [ ]
+  };
+}
+
+function generateResponse (exchange) {
+  return {
+    is: {
+      statusCode: exchange.responseStatusCode,
+      headers: exchange.responseHeaders,
+      body: exchange.responseBody
+    }
+  }
+}
+
 var MountebankGenerator = function () {
 
   this.generate = function (context, requests, options) {
-    var clientCode = "testcode";
-    return clientCode;
+    var mountebankEndpoint = generateEmpty();
+    requests.forEach(function (req) {
+
+      mountebankEndpoint.predicates.push({
+        equals: {
+          method: req.method,
+          path: pathFromUrl(req.url),
+          headers: req.headers,
+        }
+      });
+
+      var lastExchange = req.getLastExchange();
+      mountebankEndpoint.responses.push(generateResponse(lastExchange));
+
+    });
+    return JSON.stringify(mountebankEndpoint, null, "  ");
   }
 
 };
